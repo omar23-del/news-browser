6064cdb036bf8a71bc97190050e4e8d6ec0ce21a
omar-12 <ok2903474@gmail.com>
2025-05-18 09:47:08 +0300

    add proxy service

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 let apikey = "87bbbb72ead5407c851957c6a0d33751";
 let apiurl = "https://newsapi.org/v2/everything?q=";
+let proxyurl = "https://api.allorigins.win/raw?url=";
 let cardcont = document.querySelector(".card-cont");
-let fnav = document.getElementById("f")
-let bnav = document.getElementById("b")
-let nav = document.querySelectorAll("li")
+let fnav = document.getElementById("f");
+let bnav = document.getElementById("b");
+let nav = document.querySelectorAll("li");
 
 window.addEventListener("DOMContentLoaded", () => {
   fetchnews("european football");
 });
 
 async function fetchnews(query) {
-  let response = await fetch(`${apiurl}${encodeURIComponent(query)}&apikey=${apikey}`);
-  let data = await response.json();
-  console.log(data);
-  displaynews(data.articles);
+  let url = `${apiurl}${encodeURIComponent(query)}&apiKey=${apikey}`; 
+  let proxyRequestUrl = proxyurl + encodeURIComponent(url);
+  try {
+    let response = await fetch(proxyRequestUrl);
+    let data = await response.json();
+    console.log(data);
+    displaynews(data.articles);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    cardcont.innerHTML = "<p>Failed to fetch news. Please try again later.</p>";
+  }
 }
+
 
 function displaynews(articles) {
   articles.forEach((article) => {
@@ -28,12 +37,12 @@ function displaynews(articles) {
         `;
     cardcont.appendChild(div);
     div.addEventListener("click", () => {
-        window.open(article.url, "_blank")
-    })
+      window.open(article.url, "_blank");
+    });
   });
 }
 
 function navItem(id) {
-    cardcont.innerHTML = ""
-    fetchnews(id)
+  cardcont.innerHTML = "";
+  fetchnews(id);
 }
